b6f721a16ea785bb6cdd5daed0a7552fd8f705fd
Sander Kooijmans <sander@gogognome.nl>
2015-12-14 23:05:34 +0100

    Fixed javadoc errors and indentation.

--- src/main/java/nl/gogognome/lib/swing/WidgetFactory.java
+++ src/main/java/nl/gogognome/lib/swing/WidgetFactory.java
@@ -49,102 +49,103 @@ import org.slf4j.LoggerFactory;
  */
 public class WidgetFactory {
 
-	private final static Logger LOGGER = LoggerFactory.getLogger(WidgetFactory.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(WidgetFactory.class);
 
-	/** The <code>TextResource</code> used to obtain string resources. */
-	private TextResource textResource;
+    /** The <code>TextResource</code> used to obtain string resources. */
+    private TextResource textResource;
 
-	private Map<String, Icon> iconCache = new HashMap<String, Icon>();
-	private Map<String, Image> imageCache = new HashMap<String, Image>();
+    private Map<String, Icon> iconCache = new HashMap<String, Icon>();
+    private Map<String, Image> imageCache = new HashMap<String, Image>();
 
-	public WidgetFactory(TextResource textResource)
-	{
-		this.textResource = textResource;
-	}
+    public WidgetFactory(TextResource textResource)
+    {
+        this.textResource = textResource;
+    }
 
-	/**
-	 * Creates an <code>Action</code> for the specified identifier.
-	 * @param id the identifier
-	 * @return the <code>Action</code> or <code>null</code> if the specified
-	 *          identifier does not occur in the resources.
-	 */
-	public ActionWrapper createAction(String id) {
+    /**
+     * Creates an <code>Action</code> for the specified identifier.
+     * @param id the identifier
+     * @return the <code>Action</code> or <code>null</code> if the specified
+     *          identifier does not occur in the resources.
+     */
+    public ActionWrapper createAction(String id) {
 
-	    String name = textResource.getString(id);
+        String name = textResource.getString(id);
 
-	    ActionWrapper action;
-	    if (name != null) {
-		    Icon icon = createIcon(id + ".icon16");
-	        if (icon != null) {
-	            action = new ActionWrapper(name, icon);
-	        } else {
-	            action = new ActionWrapper(name);
-	        }
-	    } else {
-	        action = new ActionWrapper();
-	    }
+        ActionWrapper action;
+        if (name != null) {
+            Icon icon = createIcon(id + ".icon16");
+            if (icon != null) {
+                action = new ActionWrapper(name, icon);
+            } else {
+                action = new ActionWrapper(name);
+            }
+        } else {
+            action = new ActionWrapper();
+        }
 
-	    String s = textResource.getString(id + ".accelerator");
-	    KeyStroke accelerator = s != null ? KeyStroke.getKeyStroke(s) : null;
-	    if (accelerator != null) {
-	        action.putValue(Action.ACCELERATOR_KEY, accelerator);
-	    }
+        String s = textResource.getString(id + ".accelerator");
+        KeyStroke accelerator = s != null ? KeyStroke.getKeyStroke(s) : null;
+        if (accelerator != null) {
+            action.putValue(Action.ACCELERATOR_KEY, accelerator);
+        }
 
-	    s = textResource.getString(id + ".mnemonic");
-	    int mnemonic = getMnemonic(id);
-	    if (mnemonic != -1) {
-	        action.putValue(Action.MNEMONIC_KEY, new Integer(mnemonic));
-	    }
+        s = textResource.getString(id + ".mnemonic");
+        int mnemonic = getMnemonic(id);
+        if (mnemonic != -1) {
+            action.putValue(Action.MNEMONIC_KEY, new Integer(mnemonic));
+        }
 
-	    s = textResource.getString(id + ".tooltip");
-	    if (s != null) {
-	        action.putValue(Action.SHORT_DESCRIPTION, s);
-	    }
+        s = textResource.getString(id + ".tooltip");
+        if (s != null) {
+            action.putValue(Action.SHORT_DESCRIPTION, s);
+        }
 
-	    s = textResource.getString(id + ".contexthelp");
-	    if (s != null) {
-	        action.putValue(Action.LONG_DESCRIPTION, s);
-	    }
+        s = textResource.getString(id + ".contexthelp");
+        if (s != null) {
+            action.putValue(Action.LONG_DESCRIPTION, s);
+        }
 
-	    return action;
-	}
+        return action;
+    }
 
-	/**
-	 * Creates a button.
-	 * @param id the id of the button's description in the resources.
-	 * @param the action to be performed when the button is pressed
-	 * @return the button.
-	 */
-	public JButton createButton(String id, Action action) {
-	    ActionWrapper actionWrapper = createAction(id);
-	    actionWrapper.setAction(action);
+    /**
+     * Creates a button.
+     * @param id the id of the button's description in the resources.
+     * @param action the action to be performed when the button is pressed
+     * @return the button.
+     */
+    public JButton createButton(String id, Action action) {
+        ActionWrapper actionWrapper = createAction(id);
+        actionWrapper.setAction(action);
 
-		return new JButton(actionWrapper);
-	}
+        return new JButton(actionWrapper);
+    }
 
-	/**
-	 * Creates a button containing an icon only.
-	 * @param id the id of the button's description in the resources.
-	 * @param the action to be performed when the button is pressed
-	 * @param size the width and height in pixels
-	 * @return the button.
-	 */
-	public JButton createIconButton(String id, Action action, int size) {
-	    JButton button = createButton(id, action);
-	    button.setText(null);
-	    button.setPreferredSize(new Dimension(size, size));
-		return button;
-	}
+    /**
+     * Creates a button containing an icon only.
+     * @param id the id of the button's description in the resources.
+     * @param action the action to be performed when the button is pressed
+     * @param size the width and height in pixels
+     * @return the button.
+     */
+    public JButton createIconButton(String id, Action action, int size) {
+        JButton button = createButton(id, action);
+        button.setText(null);
+        button.setToolTipText(textResource.getString(id));
+        button.setPreferredSize(new Dimension(size, size));
+        return button;
+    }
 
-	/**
-	 * Creates a label with the specified text.
-	 *
-	 * @param id the id of the text.
-	 * @return the label.
-	 */
-	public JLabel createLabel(String id) {
-		return createLabel(id, null);
-	}
+    /**
+     * Creates a label with the specified text.
+     *
+     * @param id the id of the text.
+     * @return the label.
+     */
+    public JLabel createLabel(String id) {
+        return createLabel(id, null);
+    }
 
     /**
      * Creates a label with the specified text. If <code>component != null</code> then this
@@ -167,43 +168,43 @@ public class WidgetFactory {
         return label;
     }
 
-	/**
-	 * Creates a text field. When the created text field gains focus, its
-	 * contents is selected.
-	 *
-	 * @return the text field.
-	 */
-	public JTextField createTextField() {
-		JTextField textField = new JTextField();
-		return textField;
-	}
+    /**
+     * Creates a text field. When the created text field gains focus, its
+     * contents is selected.
+     *
+     * @return the text field.
+     */
+    public JTextField createTextField() {
+        JTextField textField = new JTextField();
+        return textField;
+    }
 
-	/**
-	 * Creates a text field. When the created text field gains focus, its
-	 * contents is selected.
-	 *
-	 * @param columns the number of columns of the text field.
-	 * @return the text field.
-	 */
-	public JTextField createTextField(int columns)
-	{
-		JTextField textField = new JTextField(columns);
+    /**
+     * Creates a text field. When the created text field gains focus, its
+     * contents is selected.
+     *
+     * @param columns the number of columns of the text field.
+     * @return the text field.
+     */
+    public JTextField createTextField(int columns)
+    {
+        JTextField textField = new JTextField(columns);
         textField.setFocusable(true);
-		return textField;
-	}
+        return textField;
+    }
 
-	/**
-	 * Creates a text field. When the created text field gains focus, its
-	 * contents is selected.
-	 *
-	 * @param text the initial text of the text field.
-	 * @return the text field.
-	 */
-	public JTextField createTextField(String text)
-	{
-		JTextField textField = new JTextField(text);
-		return textField;
-	}
+    /**
+     * Creates a text field. When the created text field gains focus, its
+     * contents is selected.
+     *
+     * @param text the initial text of the text field.
+     * @return the text field.
+     */
+    public JTextField createTextField(String text)
+    {
+        JTextField textField = new JTextField(text);
+        return textField;
+    }
 
     /**
      * Creates a check box.
@@ -218,54 +219,53 @@ public class WidgetFactory {
         return checkBox;
     }
 
-	/**
-	 * Creates a <tt>JMenu</tt> with a specified string as label.
-	 * @param id the id of the string.
-	 * @param keyEvent the key event assoicated with the mnemonic.
-	 * @return the menu.
-	 */
-	public JMenu createMenu(String id)
-	{
-		JMenu result = new JMenu(textResource.getString(id));
-		int mnemonic = getMnemonic(id);
-		if (mnemonic != -1)
-		{
-			result.setMnemonic(mnemonic);
-		}
-		return result;
-	}
+    /**
+     * Creates a <tt>JMenu</tt> with a specified string as label.
+     * @param id the id of the string.
+     * @return the menu.
+     */
+    public JMenu createMenu(String id)
+    {
+        JMenu result = new JMenu(textResource.getString(id));
+        int mnemonic = getMnemonic(id);
+        if (mnemonic != -1)
+        {
+            result.setMnemonic(mnemonic);
+        }
+        return result;
+    }
 
-	/**
-	 * Creates a <tt>JMenuItem</tt> with a specified string as label.
-	 * @param id the id of the string.
-	 * @param l the action listener that has to be added to the menu item or
-	 *          <tt>null</tt> if no listener has to be added.
-	 * @return the menu item.
-	 */
-	public JMenuItem createMenuItem(String id, ActionListener l)
-	{
-		JMenuItem result = new JMenuItem(textResource.getString(id));
-		result.setActionCommand(id);
-		int mnemonic = getMnemonic(id);
-		if (mnemonic != -1)
-		{
-		    result.setMnemonic(mnemonic);
-		}
-		String keyStrokeDescription = textResource.getString(id + ".keystroke");
-		if (keyStrokeDescription != null)
-		{
-			KeyStroke keyStroke = KeyStroke.getKeyStroke(keyStrokeDescription);
-			if (keyStroke != null)
-			{
-				result.setAccelerator(keyStroke);
-			}
-		}
-		if (l != null)
-		{
-			result.addActionListener(l);
-		}
-		return result;
-	}
+    /**
+     * Creates a <tt>JMenuItem</tt> with a specified string as label.
+     * @param id the id of the string.
+     * @param l the action listener that has to be added to the menu item or
+     *          <tt>null</tt> if no listener has to be added.
+     * @return the menu item.
+     */
+    public JMenuItem createMenuItem(String id, ActionListener l)
+    {
+        JMenuItem result = new JMenuItem(textResource.getString(id));
+        result.setActionCommand(id);
+        int mnemonic = getMnemonic(id);
+        if (mnemonic != -1)
+        {
+            result.setMnemonic(mnemonic);
+        }
+        String keyStrokeDescription = textResource.getString(id + ".keystroke");
+        if (keyStrokeDescription != null)
+        {
+            KeyStroke keyStroke = KeyStroke.getKeyStroke(keyStrokeDescription);
+            if (keyStroke != null)
+            {
+                result.setAccelerator(keyStroke);
+            }
+        }
+        if (l != null)
+        {
+            result.addActionListener(l);
+        }
+        return result;
+    }
 
     /**
      * Creates a <tt>JComboBox</tt> with the specified values.
@@ -291,8 +291,8 @@ public class WidgetFactory {
      * @return the icon or <code>null</code> if no icon exists with the specified id
      */
     public Icon createIcon(String id) {
-    	Icon icon = iconCache.get(id);
-    	if (icon == null) {
+        Icon icon = iconCache.get(id);
+        if (icon == null) {
             URL iconUrl = getUrlForResource(id);
             if (iconUrl == null) {
                 return null;
@@ -305,7 +305,7 @@ public class WidgetFactory {
                 icon = new ImageIcon(iconUrl);
             }
             iconCache.put(id, icon);
-    	}
+        }
         return icon;
     }
 
@@ -318,20 +318,20 @@ public class WidgetFactory {
      * @return the icon or <code>null</code> if no icon exists with the specified id
      */
     public Image createImage(String id) {
-    	Image image = imageCache.get(id);
-    	if (image == null) {
+        Image image = imageCache.get(id);
+        if (image == null) {
             URL imageUrl = getUrlForResource(id);
             if (imageUrl == null) {
                 return null;
             }
 
             try {
-				image = ImageIO.read(imageUrl);
-			} catch (IOException e) {
-				LOGGER.warn("Failed to load image " + imageUrl + ": " + e.getMessage(), e);
-			}
+                image = ImageIO.read(imageUrl);
+            } catch (IOException e) {
+                LOGGER.warn("Failed to load image " + imageUrl + ": " + e.getMessage(), e);
+            }
             imageCache.put(id, image);
-    	}
+        }
         return image;
     }
 
@@ -348,28 +348,28 @@ public class WidgetFactory {
         if (resourceName == null) {
             return null;
         } else {
-        	return getClass().getResource(resourceName);
+            return getClass().getResource(resourceName);
         }
     }
 
     /**
      * Creates a title border for the specified title.
      * @param titleId the id of the title
-	 * @param arguments the arguments
+     * @param args the arguments
      * @return the title border
      */
     public Border createTitleBorder(String titleId, Object... args) {
-    	return new TitledBorder(' '  + textResource.getString(titleId, args) + ' ');
+        return new TitledBorder(' '  + textResource.getString(titleId, args) + ' ');
     }
 
     /**
      * Creates a title border for the specified title.
      * @param titleId the id of the title
-	 * @param arguments the arguments
+     * @param args the arguments
      * @return the title border
      */
     public Border createTitleBorderWithPadding(String titleId, Object... args) {
-    	return new CompoundBorder(createTitleBorder(titleId, args),
+        return new CompoundBorder(createTitleBorder(titleId, args),
                 new EmptyBorder(10, 10, 10, 10));
     }
 
@@ -379,8 +379,8 @@ public class WidgetFactory {
      * @return the title border
      */
     public Border createTitleBorderWithMarginAndPadding(String titleId) {
-    	return new CompoundBorder(new EmptyBorder(10, 10, 10, 10),
-    		createTitleBorderWithPadding(titleId));
+        return new CompoundBorder(new EmptyBorder(10, 10, 10, 10),
+            createTitleBorderWithPadding(titleId));
     }
 
     /**
@@ -391,9 +391,9 @@ public class WidgetFactory {
      * @return the table
      */
     public JTable createTable(AbstractTableModel tableModel) {
-    	JTable table = new JTable(tableModel);
-    	initTableColumns(table, tableModel);
-    	return table;
+        JTable table = new JTable(tableModel);
+        initTableColumns(table, tableModel);
+        return table;
     }
 
     /**
@@ -404,15 +404,15 @@ public class WidgetFactory {
      * @return the table
      */
     public JTable createSortedTable(AbstractTableModel tableModel) {
-    	JTable table = new JTable(tableModel);
-    	TableRowSorter<AbstractTableModel> sorter = new TableRowSorter<AbstractTableModel>(tableModel);
-    	initTableColumns(table, tableModel);
-    	initSorterForTableModel(sorter, tableModel);
-    	table.setRowSorter(sorter);
-    	return table;
+        JTable table = new JTable(tableModel);
+        TableRowSorter<AbstractTableModel> sorter = new TableRowSorter<AbstractTableModel>(tableModel);
+        initTableColumns(table, tableModel);
+        initSorterForTableModel(sorter, tableModel);
+        table.setRowSorter(sorter);
+        return table;
     }
 
-	private void initTableColumns(JTable table, AbstractTableModel tableModel) {
+    private void initTableColumns(JTable table, AbstractTableModel tableModel) {
         TableColumnModel columnModel = table.getColumnModel();
         int nrCols = tableModel.getColumnCount();
 
@@ -425,7 +425,7 @@ public class WidgetFactory {
             // If present, set the table cell renderer
             TableCellRenderer renderer = tableModel.getRendererForColumn(i);
             if (renderer != null) {
-            	column.setCellRenderer(renderer);
+                column.setCellRenderer(renderer);
             }
 
             // If present, set the table cell editor
@@ -434,18 +434,18 @@ public class WidgetFactory {
                 column.setCellEditor(editor);
             }
         }
-	}
+    }
 
-	private void initSorterForTableModel(TableRowSorter<AbstractTableModel> sorter,
-			AbstractTableModel tableModel) {
-		for (int c=0; c<tableModel.getColumnCount(); c++) {
-    		ColumnDefinition colDef = tableModel.getColumnDefinition(c);
-    		Comparator<?> comparator =  colDef.getComparator();
-    		if (comparator != null) {
-    			sorter.setComparator(c, comparator);
-    		}
-    	}
-	}
+    private void initSorterForTableModel(TableRowSorter<AbstractTableModel> sorter,
+            AbstractTableModel tableModel) {
+        for (int c=0; c<tableModel.getColumnCount(); c++) {
+            ColumnDefinition colDef = tableModel.getColumnDefinition(c);
+            Comparator<?> comparator =  colDef.getComparator();
+            if (comparator != null) {
+                sorter.setComparator(c, comparator);
+            }
+        }
+    }
 
     /**
      * Creates a scroll pane for the component.
@@ -462,7 +462,7 @@ public class WidgetFactory {
      * Creates a scroll pane for the component with a titled border.
      * @param component the component
      * @param titleId the id of the title
-	 * @param arguments the arguments
+     * @param args the arguments
      * @return the scroll pane
      */
     public JScrollPane createScrollPane(Component component, String titleId, Object... args) {
@@ -479,19 +479,19 @@ public class WidgetFactory {
     private int getMnemonic(String id)
     {
         int result = -1;
-		try
-		{
-			String mnemonicId = id + ".mnemonic";
-			if (textResource.containsString(mnemonicId)) {
-				Class<?> clazz = KeyEvent.class;
-			    Field field = clazz.getField(textResource.getString(mnemonicId));
-				result = field.getInt(null);
-			}
-		}
-		catch (Exception e)
-		{
-		    // no mnemonic available in the resources.
-		}
-		return result;
+        try
+        {
+            String mnemonicId = id + ".mnemonic";
+            if (textResource.containsString(mnemonicId)) {
+                Class<?> clazz = KeyEvent.class;
+                Field field = clazz.getField(textResource.getString(mnemonicId));
+                result = field.getInt(null);
+            }
+        }
+        catch (Exception e)
+        {
+            // no mnemonic available in the resources.
+        }
+        return result;
     }
 }
